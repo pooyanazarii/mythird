233eeb86c6faf8129a96daf9f24a6fe710d9e7ae
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-22 01:09:46 +0430

    Added html and css file
    to general templates and static folder

--- mypages/urls.py
+++ mypages/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from mypages.views import *
-
+app_name="mypages"
 urlpatterns = [
-    path('view_v',view_v),
-
+    path('',view_index,name='index'),
+    path('about',view_about,name='about'),
+    path('class',view_class,name='class'),
+    path('contact',view_contact,name='contact'),
+    path('gallery',view_gallery,name='gallery'),
+    path('single',view_single,name='single'),
+    path('team',view_team,name='team'),
 ]
--- mypages/views.py
+++ mypages/views.py
@@ -1,5 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
-def view_v(request):
-    return render(request,'home.html')
+
+def view_index(request):
+    return render(request,'mypages/index.html')
+def view_about(request):
+    return render(request,'mypages/about.html')
+
+def view_class(request):
+    return render(request,'mypages/class.html')
+    
+def view_contact(request):
+    return render(request,'mypages/contact.html')
+    
+def view_gallery(request):
+    return render(request,'mypages/gallery.html')
+    
+def view_single(request):
+    return render(request,'mypages/single.html')
+    
+def view_team(request):
+    return render(request,'mypages/team.html')
